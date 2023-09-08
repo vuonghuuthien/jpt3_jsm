c8aa229b3ad94d19b784f6b83b18bcdaa2ac13f7
thien.vuong <thien.vuong@nois.vn>
2023-09-09 02:19:28 +0700

    Add Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import React from 'react'
+import React from 'react';
+
+// import Article from './components/article/Article';
+// import Brand from './components/brand/Brand';
+// import Cta from './components/cta/Cta';
+// import Feature from './components/feature/Feature';
+// import Navbar from './components/navbar/Navbar';
+import { Article, Brand, Cta, Feature, Navbar } from './components';
+
 
 const App = () => {
   return (
