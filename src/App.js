acb0bfdf214f2666ccb26663a3bb54b640bce201
thien.vuong <thien.vuong@nois.vn>
2023-09-09 02:55:49 +0700

    Add App.css and index.css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import './App.css';
 
-// import Article from './components/article/Article';
-// import Brand from './components/brand/Brand';
-// import Cta from './components/cta/Cta';
-// import Feature from './components/feature/Feature';
-// import Navbar from './components/navbar/Navbar';
 import { Article, Brand, Cta, Feature, Navbar } from './components';
-
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
 
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer />
     </div>
   )
 }
